14387f67eb34db8d4e31af01aa7894875bf276c0
Viewbell <128227140+Viewbell@users.noreply.github.com>
2023-06-23 22:37:43 +0200

    Add files via upload

---
+++ ćwiczenie 1.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int licznik = 0;
+        int i = 2;
+
+        while (licznik < n)
+        {
+            bool czyLiczbaPierwsza = true;
+
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    czyLiczbaPierwsza = false;
+                }
+            }
+            if (czyLiczbaPierwsza)
+            {
+                licznik++;
+                Console.Write(i + ", ");
+            }
+            i++;
+        }
+    }
+}
---
+++ ćwiczenie 2_2.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int n = 10;
+        Random random = new Random();
+        int licznikLosowan = 0;
+        int iloscLiczbDoWylosowania = n;
+
+        for (int iloscLiczbWylosowanych = 0; iloscLiczbWylosowanych < iloscLiczbDoWylosowania; iloscLiczbWylosowanych++)
+        {
+            int wylosowanaLiczba;
+
+            wylosowanaLiczba = random.Next(-2, 7);
+            wylosowanaLiczba *= 4;
+            licznikLosowan++;
+
+            Console.Write(wylosowanaLiczba);
+
+            if (iloscLiczbWylosowanych < iloscLiczbDoWylosowania - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+
+        Console.WriteLine("\n\nLicznik liczb wylosowanych: " + licznikLosowan);
+    }
+}
